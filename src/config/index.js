35ad4fd5e813b3edbda7f501fcbb4189c97388a2
Shir Brunner <shirbrunner@gmail.com>
2018-09-24 23:15:30 +0300

    fixed events

--- src/config/index.js
+++ src/config/index.js
@@ -3,7 +3,7 @@ const ms = require('ms');
 module.exports = {
     port: 4001,
     levelEditorUrl: 'http://localhost:4000',
-    roomTimeout: ms('1 seconds'),
+    roomTimeout: ms('6 seconds'),
     debug: {
         latency: process.env.DEBUG_LATENCY || 0,
     }
--- src/lib/game/index.js
+++ src/lib/game/index.js
@@ -2,8 +2,6 @@ const commonConfig = require('../../../../client/src/game/common_config');
 const Loop = require('../../utils/loop');
 const World = require('../../../../client/src/game/engine/world');
 const InputHandler = require('./input_handler');
-const EventsProcessor = require('../../../../client/src/game/engine/events/events_processor');
-const GameplayFactory = require('../../../../client/src/game/gameplay/gameplay_factory');
 const _ = require('lodash');
 const FRAME_RATE = Math.round(1000 / commonConfig.fps);
 
@@ -11,8 +9,6 @@ module.exports = class Game {
     constructor(level, clients) {
         this.world = World.create(level, clients.map(client => client.toObject()));
         this.inputHandler = new InputHandler(this.world);
-        this.eventsProcessor = new EventsProcessor(this.world);
-        this.gameplay = (new GameplayFactory()).getGameplay(this.world);
         this.clients = clients;
     }
 
@@ -28,19 +24,17 @@ module.exports = class Game {
 
     _loop(deltaTime, currentFrame) {
         let deltaFrames = deltaTime / FRAME_RATE;
-        for (let frame = deltaFrames; frame >= 1; frame--) {
+        for (let frame = deltaFrames; frame >= 1; frame--)
             this._update(currentFrame - frame);
-        }
     }
 
     _update(currentFrame) {
         this.inputHandler.applyInputs();
         this.world.update(1, currentFrame);
         this._sendSharedStateIfChanged();
-        let events = this.world.worldEvents.collectEvents();
+        let events = this.world.gameplay.collectEvents();
         if (events.length) {
-            this.eventsProcessor.process(events);
-            this.gameplay.update(events);
+            this.world.gameplay.applyEvents(events);
             this.clients.forEach(client => client.send('EVENTS', events));
             this._sendSharedState();
         }
@@ -62,7 +56,7 @@ module.exports = class Game {
 
     _onClientLeft(clientToRemove) {
         this.clients = this.clients.filter(client => client.id !== clientToRemove.id);
-        this.world.worldEvents.addEvent('PLAYER_LEFT', { playerId: clientToRemove.id });
+        this.world.gameplay.addEvent('PLAYER_LEFT', { playerId: clientToRemove.id });
 
         if(!this.clients.length) {
             console.log('Game has been stopped because all clients left');
